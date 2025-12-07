5dfc9135efca19ad5fd8373fe56b4758bd824e4e
agrz4 <agramahardika479@gmail.com>
2025-12-07 20:38:21 +0700

    feat(service): implement ListProducts service with repository delegation

--- products/handlers.go
+++ products/handlers.go
@@ -1,5 +1,11 @@
 package products
 
+import (
+	"ecom_go_api/internal/json"
+	"log"
+	"net/http"
+)
+
 type handler struct {
 	service Service
 }
@@ -8,4 +14,14 @@ func NewHandler(service Service) *handler {
 	return &handler{
 		service: service,
 	}
+}
+
+func (h *handler) ListProducts(w http.ResponseWriter, r *http.Request) {
+	products, err := h.service.ListProducts(r.Context())
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.Write(w, http.StatusOK, products)
 }
--- products/service.go
+++ products/service.go
@@ -1,4 +1,22 @@
 package products
 
+import (
+	"context"
+	repo "ecom_go_api/internal/adapters/postgresql/sqlc"
+)
+
 type Service interface {
+	ListProducts(ctx context.Context) ([]repo.Product, error)
+}
+
+type svc struct {
+	repo repo.Querier
+}
+
+type NewService(repo repo.Querier) Service {
+	return &svc{repo: repo}
+}
+
+func (s *svc) ListProducts(ctx context.Context) ([]repo.Product, error) {
+	return s.repo.ListProducts(ctx)
 }
